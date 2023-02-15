86ef9dfc1cfb733d64f7787a2d6c32159fa14a41
Moo Plah Soe Kusay <plahsoekusay@gmail.com>
2023-02-16 12:19:10 +1300

    game_history: update game history list view.
    add more info to cards.
    add scroll bar.

--- lib/game_history/game_history_page.dart
+++ lib/game_history/game_history_page.dart
@@ -1,5 +1,6 @@
 import 'package:chess_bored/app/bloc/game_history_bloc.dart';
 import 'package:chess_bored/game_history/widgets/game_preview.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_chess_board/flutter_chess_board.dart';
@@ -28,35 +29,63 @@ class _GameHistoryPageState extends State<GameHistoryPage> {
       body: BlocBuilder<GameHistoryBloc, GameHistoryState>(
         builder: (context, state) {
           return state is GameHistoryLoadedState
-              ? ListView.builder(
-                  physics: const BouncingScrollPhysics(
-                    parent: AlwaysScrollableScrollPhysics(),
-                  ),
-                  itemCount: state.games.length,
-                  itemBuilder: (context, index) {
-                    ChessBoardController controller = ChessBoardController();
-                    controller.loadFen(state.games[index].gameAsFen);
-                    return Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Card(
+              ? CupertinoScrollbar(
+                  child: ListView.builder(
+                    physics: const BouncingScrollPhysics(
+                      parent: AlwaysScrollableScrollPhysics(),
+                    ),
+                    itemCount: state.games.length,
+                    itemBuilder: (context, index) {
+                      ChessBoardController controller = ChessBoardController();
+                      controller.loadFen(state.games[index].gameAsFen);
+                      return Card(
                         elevation: 2,
                         clipBehavior: Clip.hardEdge,
                         child: InkWell(
                           onTap: () {},
-                          child: Center(
-                            child: SizedBox(
-                              width: 200,
-                              height: 200,
-                              child: GamePreview(
-                                fen: state.games[index].gameAsFen,
-                                theme: state.games[index].gameBoardTheme,
-                              ),
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                SizedBox(
+                                  width: MediaQuery.of(context).size.width / 2,
+                                  child: GamePreview(
+                                    fen: state.games[index].gameAsFen,
+                                    theme: state.games[index].gameBoardTheme,
+                                  ),
+                                ),
+                                Expanded(
+                                  child: Column(
+                                    children: [
+                                      Text(
+                                          state.games[index]
+                                                      .gameClockSettings !=
+                                                  null
+                                              ? "${state.games[index].gameClockSettings!.startTime} + ${state.games[index].gameClockSettings!.incrementTime}"
+                                              : '-',
+                                          style: Theme.of(context)
+                                              .textTheme
+                                              .bodyMedium),
+                                      Text(state.games[index].winStatus.title,
+                                          style: Theme.of(context)
+                                              .textTheme
+                                              .headlineSmall),
+                                      Text(state.games[index].gameResult.title,
+                                          style: Theme.of(context)
+                                              .textTheme
+                                              .bodyLarge),
+                                    ],
+                                  ),
+                                )
+                              ],
                             ),
                           ),
                         ),
-                      ),
-                    );
-                  })
+                      );
+                    },
+                  ),
+                )
               : Container();
         },
       ),
